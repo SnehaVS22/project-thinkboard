81735a9c25e46c118c236ce02c034b1057ed241a
Sneha <141664785+Heather1238@users.noreply.github.com>
2025-06-23 14:07:35 +0530

    prepared for the deployment

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,22 +3,24 @@ import notesRoutes from "./routes/notesRoutes.js"
 import{ connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
-import cors from "cors"
+import cors from "cors";
+import path from "path";
 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const _dirname=path.resolve();
 
 //here was the original ie first place where we placed connectDB();
 
-
+if(process.env.NODE_ENV !== "production"){
 app.use(
     cors({
         origin:"http://localhost:5173",
     })
-);
+);};
 //middleware
 app.use(express.json());// this will parse JSON bodies:will allow access to req.body
 
@@ -29,6 +31,15 @@ app.use(express.json());// this will parse JSON bodies:will allow access to req.
 //});
 app.use(rateLimiter);
 app.use("/api/notes",notesRoutes);
+//{
+if(process.env.NODE_ENV === "production"){
+//middleware from express these are used to deploy it without having cors errors
+app.use(express.static(path.join(_dirname,"../front-end/dist")));
+//if any other rather than api/notes
+app.get("*",(req,res) => {
+    res.sendFile(path.join(_dirname,"../front-end","dist","index.html"))
+});};
+//}only if it is in production ie,its own render.com
 app.use(cors());
 
 connectDB().then(()=>{
--- front-end/src/lib/axios.js
+++ front-end/src/lib/axios.js
@@ -1,9 +1,9 @@
 //to make http://localhost5001/api/notes
 
 import axios from "axios";
-
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" :"/api"
 const api=axios.create({
-    baseURL:"http://localhost:5001/api"
+    baseURL:BASE_URL //in production we don't know specific local host therefore we make this dynamic 
 });
 
 export default api;
