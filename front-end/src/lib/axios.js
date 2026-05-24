877629cbf7350bb681c5945e1af3d0dc9ee0edfe
Sneha <141664785+Heather1238@users.noreply.github.com>
2026-05-24 14:15:01 +0530

    fix: clean up axios base URL config

--- front-end/src/lib/axios.js
+++ front-end/src/lib/axios.js
@@ -1,9 +1,11 @@
 //to make http://localhost5001/api/notes
 
 import axios from "axios";
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" :"/api"
+//const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" :"/api"
 const api=axios.create({
-    baseURL:BASE_URL //in production we don't know specific local host therefore we make this dynamic 
+   // baseURL:BASE_URL in production we don't know specific local host therefore we make this dynamic 
+   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
 });
 
 export default api;
+
