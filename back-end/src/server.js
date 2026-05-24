b88255ba744125c77fa2bc4b56d6c0d2ab16bcf9
Sneha <141664785+Heather1238@users.noreply.github.com>
2026-05-24 14:23:26 +0530

    fix: add vercel URL to CORS allowed origins

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -12,7 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const _dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+//app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://project-thinkboard.vercel.app"
+  ],
+}));
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
